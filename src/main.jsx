824b701c4bb508013ba3b7d57e725e6fa556f4f8
JovanyMolina <s20120208@alumnos.itsur.edu.mx>
2024-12-19 18:55:37 -0600

    practica terminada de Tailwind

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css';
+
+function main() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center">Hello, world!</h1>
+    </div>
+  );
+}
